cab7c9c61875b2880a2b4c975c987559e3adcf23
onframe96 <37874467+onframe96@users.noreply.github.com>
2018-03-31 15:43:26 +0800

    Add files via upload

---
+++ templates/parts/site-title.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Displays the site title.
+ *
+ * @package Primer
+ * @since   1.0.0
+ */
+
+?>
+
+<div class="site-title-wrapper">
+
+	<?php primer_the_custom_logo(); ?>
+
+	<?php primer_the_site_description(); ?>
+
+</div><!-- .site-title-wrapper -->
